04e30ce59b01c801cfd142c329266250e474d725
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-04-22 10:37:24 +1200

    add stub

---
+++ components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Milk</h2>
+        <h2>Tea</h2>
+        <h2>Cleaning</h2>
+        <h2>Dishwasher</h2>
+      </div>
+      )
+  }
+}
+
+export default App
