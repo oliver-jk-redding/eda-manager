14a4a52b7a4f5e6ef04ba8b37b852eeed5d433b0
oliver-jk-redding <oliver.jk.redding@gmail.com>
2016-04-22 16:32:32 +1200

    dhfkjsa

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("welcome to eda-manager")
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import HotWaterQueue from './components/HotWaterQueue'
+
+render(
+  <HotWaterQueue />
+  , document.querySelector('main')
+  )
